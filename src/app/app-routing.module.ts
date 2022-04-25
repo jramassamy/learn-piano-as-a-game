bd2f9a34c1bd69f12adf101a6e4e194f5ae274f1
commercial <commercial@seo-reborn.com>
2022-04-25 18:49:09 +0200

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: 'home', component: AppComponent },
+  { path: '', component: AppComponent },
   {
-    path: 'home/:idExercice',
+    path: '/:idExercice',
     component: AppComponent
   },
   {
     path: '**',
-    redirectTo: 'home'
+    redirectTo: ''
   }
 ];
 
